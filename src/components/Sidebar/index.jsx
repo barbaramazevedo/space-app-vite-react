6f5c5ec914d08f6094c071c28a873dbce9dfb798
Bárbara Azevedo <barbara.azevedo22@gmail.com>
2025-05-20 19:32:43 -0300

    feat: component Sidebar implementation

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import { styled } from "styled-components";
 import NavegationItem from "./NavegationItem";
 
 const StyledList = styled.ul`
@@ -23,28 +23,24 @@ const Sidebar = () => {
                     <NavegationItem 
                         activeIcon="/icones/mais-vistas-ativo.png" 
                         inactiveIcon="/icones/mais-vistas-inativo.png"
-                        active={false}
                     >
                         Most viewed
                     </NavegationItem>
                     <NavegationItem 
                         activeIcon="/icones/mais-curtidas-ativo.png" 
                         inactiveIcon="/icones/mais-curtidas-inativo.png"
-                        active={false}
                     >
                         Most liked
                     </NavegationItem>
                     <NavegationItem 
                         activeIcon="/icones/novas-ativo.png" 
                         inactiveIcon="/icones/novas-inativo.png"
-                        active={false}
                     >
                         New
                     </NavegationItem>
                     <NavegationItem 
                         activeIcon="/icones/surpreenda-me-ativo.png" 
                         inactiveIcon="/icones/surpreenda-me-inativo.png"
-                        active={false}
                     >
                         Surprise me
                     </NavegationItem>
