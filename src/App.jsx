3d7f1785a71b03740dcfa4f5b68c5c60442b52b9
Bárbara Azevedo <barbara.azevedo22@gmail.com>
2025-05-20 10:01:00 -0300

    first bases

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+const BackGroundGradient =  styled.div``
+
+function App() {
+
+  return (
+    <BackGroundGradient>
+      
+    </BackGroundGradient>
+  )
+}
+
+export default App
