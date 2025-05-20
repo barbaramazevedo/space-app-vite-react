b5df5e1999050758a1a419217f76e59ca22bf582
Bárbara Azevedo <barbara.azevedo22@gmail.com>
2025-05-20 10:14:22 -0300

    feat: Background Gradient

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import styled from "styled-components"
 
-const BackGroundGradient =  styled.div``
+const BackGroundGradient =  styled.div`
+  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+`
 
 function App() {
 
