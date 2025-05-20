cad1c107abdb63ef7c35a14bd7a9ba728cfb0d25
Bárbara Azevedo <barbara.azevedo22@gmail.com>
2025-05-20 10:27:42 -0300

    feat: Global Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import GlobalStyles from "./components/GlobalStyles"
 
 const BackGroundGradient =  styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -11,6 +12,7 @@ function App() {
 
   return (
     <BackGroundGradient>
+      <GlobalStyles />
       
     </BackGroundGradient>
   )
