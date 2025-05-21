d3d7b243874caebae064b0d6c60f7e02995fde92
Bárbara Azevedo <barbara.azevedo22@gmail.com>
2025-05-21 09:50:24 -0300

    feat: gallery content

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,48 @@ import Header from "./components/Header"
 import Sidebar from "./components/Sidebar"
 import Banner from "./components/Banner"
 import bannerBackground from './assets/banner.png'
+import Gallery from "./components/Gallery"
 
 const BackGroundGradient =  styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
   width: 100%;
-  height: 100vh;
-  margin: 0;
+  min-height: 100vh;
+`
+
+const MainWrapper = styled.main`
+  display: flex;
+  gap: 100px;
+`
+
+const GalleryContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`
+
+const AppContainer = styled.div`
+  width: 1440px;
+  margin: 0 auto;
+  max-width: 100%;
 `
 
 function App() {
-
   return (
     <BackGroundGradient>
       <GlobalStyles />
-      <Header />
-      <Sidebar />
-      <Banner
-        text="The most complete gallery of space photos!"
-        backgroundImage={bannerBackground}
-      />
+      <AppContainer>
+        <Header />
+        <MainWrapper>
+          <Sidebar />
+          <GalleryContainer>
+            <Banner
+            text="The most complete gallery of space photos!"
+            backgroundImage={bannerBackground}
+            />
+            <Gallery/>
+          </GalleryContainer>
+        </MainWrapper>
+      </AppContainer>
     </BackGroundGradient>
   )
 }
---
+++ src/components/Gallery/index.jsx
@@ -0,0 +1,9 @@
+const Gallery = () => {
+    return (
+       <h2>
+            Browse the gallery
+       </h2> 
+    )
+}
+
+export default Gallery
