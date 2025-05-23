6f08b4ab841710bb99665bda1c22cce9a0e6e2ba
Bárbara Azevedo <barbara.azevedo22@gmail.com>
2025-05-23 20:34:54 -0300

    feat: sidebar icon click effect

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { styled } from "styled-components";
 import NavegationItem from "./NavegationItem";
 
@@ -9,6 +10,8 @@ const StyledList = styled.ul`
 `
 
 const Sidebar = ({ onSurpriseMe }) => {
+    const [activeItem, setActiveItem] = useState('home');
+
     return (
         <aside>
             <nav>
@@ -16,26 +19,35 @@ const Sidebar = ({ onSurpriseMe }) => {
                     <NavegationItem 
                         activeIcon="/icones/home-ativo.png" 
                         inactiveIcon="/icones/home-inativo.png"
-                        active={true}
+                        active={activeItem === 'home'}
+                        onClick={() => setActiveItem('home')}
                     >
                         Home
                     </NavegationItem>
                     <NavegationItem 
                         activeIcon="/icones/mais-vistas-ativo.png" 
                         inactiveIcon="/icones/mais-vistas-inativo.png"
+                        active={activeItem === 'mostViewed'}
+                        onClick={() => setActiveItem('mostViewed')}
                     >
                         Most viewed
                     </NavegationItem>
                     <NavegationItem 
                         activeIcon="/icones/mais-curtidas-ativo.png" 
                         inactiveIcon="/icones/mais-curtidas-inativo.png"
+                        active={activeItem === 'mostLiked'}
+                        onClick={() => setActiveItem('mostLiked')}
                     >
                         Most liked
                     </NavegationItem>
                     <NavegationItem 
                         activeIcon="/icones/surpreenda-me-ativo.png" 
                         inactiveIcon="/icones/surpreenda-me-inativo.png"
-                        onClick={onSurpriseMe}
+                        active={activeItem === 'surpriseMe'}
+                        onClick={() => {
+                            setActiveItem('surpriseMe');
+                            onSurpriseMe();
+                        }}
                     >
                         Surprise me
                     </NavegationItem>
